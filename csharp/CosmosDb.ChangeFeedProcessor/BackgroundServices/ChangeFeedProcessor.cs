1529078e11ca389360e0d380a85fc5ce14e84a73
axm <amocanu2000@gmail.com>
2025-01-26 16:29:37 +0000

    feat: updated change feed processor with extra config and logging

--- csharp/CosmosDb.ChangeFeedProcessor/BackgroundServices/ChangeFeedProcessor.cs
+++ csharp/CosmosDb.ChangeFeedProcessor/BackgroundServices/ChangeFeedProcessor.cs
@@ -8,6 +8,7 @@ public class ChangeProcessorService : IHostedService
     private readonly Container _container;
     private readonly IChangeFeedProcessorHandler _changeFeedProcessorHandler;
     private readonly IHostApplicationLifetime _applicationLifetime;
+    private readonly IOptionsSnapshot<ChangeProcessorOptions> _options;
     private readonly ILogger<ChangeProcessorService> _logger;
     private Microsoft.Azure.Cosmos.ChangeFeedProcessor? _processor;
 
@@ -22,16 +23,23 @@ public class ChangeProcessorService : IHostedService
         _container = cosmosClient.GetContainer(options.Value.DatabaseId, options.Value.ContainerId);
         _changeFeedProcessorHandler = changeFeedProcessorHandler;
         _applicationLifetime = applicationLifetime;
+        _options = options;
         _logger = logger;
     }
 
     public async Task StartAsync(CancellationToken cancellationToken)
     {
+        _logger.LogInformation(
+            "Starting change feed processor. Poll interval: {PollIntervalInSeconds}s.",
+            _options.Value.PollIntervalInSeconds
+        );
+
         _processor = _container
             .GetChangeFeedProcessorBuilder<object>("processorName", HandleChangesAsync)
             .WithInstanceName("hostName")
             .WithLeaseContainer(_container)
             .WithErrorNotification(ErrorDelegateAsync)
+            .WithPollInterval(TimeSpan.FromSeconds(_options.Value.PollIntervalInSeconds))
             .Build();
 
         await _processor.StartAsync();
@@ -39,14 +47,26 @@ public class ChangeProcessorService : IHostedService
 
     private async Task ErrorDelegateAsync(string leaseToken, Exception exception)
     {
-        _logger.LogError(exception, "Error occurred for lease {LeaseToken}", leaseToken);
+        switch (exception)
+        {
+            case CosmosException cosmosException:
+                if (cosmosException.StatusCode == System.Net.HttpStatusCode.TooManyRequests)
+                {
+                    _logger.LogWarning(cosmosException, "Too many requests for lease {LeaseToken}.", leaseToken);
+                    return;
+                }
+                break;
+        }
 
+        _logger.LogError(exception, "Error occurred for lease {LeaseToken}. Stopping.", leaseToken);
         _applicationLifetime.StopApplication();
         await Task.CompletedTask;
     }
 
     public async Task StopAsync(CancellationToken cancellationToken)
     {
+        _logger.LogInformation("Stopping change feed processor.");
+
         if (_processor is not null)
         {
             await _processor.StopAsync();
--- csharp/CosmosDb.ChangeFeedProcessor/BackgroundServices/ChangeProcessorOptions.cs
+++ csharp/CosmosDb.ChangeFeedProcessor/BackgroundServices/ChangeProcessorOptions.cs
@@ -4,4 +4,5 @@ public class ChangeProcessorOptions
 {
     public string DatabaseId { get; set; } = default!;
     public string ContainerId { get; set; } = default!;
+    public int PollIntervalInSeconds { get; set; } = 1;
 }
